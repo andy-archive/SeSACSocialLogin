8da5af223b8f4ab62ecc8d36d9a2ea90fb8905f7
Taekwon Lee <102043891+andy-archive@users.noreply.github.com>
2023-12-28 15:57:38 +0900

    (#1) feat: 애플 로그인 구현
    2023-12-28 (목) 수업
    
    * feat: AuthenticationServices
    
    * add: capabilities - sign in with apple
    
    * feat: decode jwt token
    
    * comment: additional logic after social login success
    
    * feat: present another view on apple login success
    
    * feat: case authorized in execution

--- SeSACSocialLogin/Sources/Application/SceneDelegate.swift
+++ SeSACSocialLogin/Sources/Application/SceneDelegate.swift
@@ -5,18 +5,42 @@
 //  Created by Taekwon Lee on 12/28/23.
 //
 
+import AuthenticationServices
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let user = UserDefaults.standard.string(forKey: "User") else {
+            print("NO USER")
+            return
+        }
+        
+        /* ⭐️ 백그라운드에서 동작 */
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(
+            forUserID: user) { credentialState, error in
+                switch credentialState {
+                case .revoked: // 탈퇴함
+                    print("revoked")
+                case .authorized: // 인증 완료
+                    print("authorized")
+                    
+                    /* ⭐️ UI는 메인 스레드에서 동작하기 */
+                    DispatchQueue.main.async {
+                        let window = UIWindow(windowScene: windowScene)
+                        window.rootViewController = MainViewController()
+                        self.window = window
+                        window.makeKeyAndVisible()
+                    }
+                default:
+                    print("DEFAULT: notFound or transferred")
+                }
+            }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- SeSACSocialLogin/Sources/Presentation/ViewController.swift
+++ SeSACSocialLogin/Sources/Presentation/ViewController.swift
@@ -5,15 +5,177 @@
 //  Created by Taekwon Lee on 12/28/23.
 //
 
+import AuthenticationServices
 import UIKit
 
-class ViewController: UIViewController {
-
+/* 임의의 새 뷰 */
+final class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue.withAlphaComponent(0.8)
     }
-
-
 }
 
+/* 📝
+ 소셜 로그인(페북/구글/카카오..), 애플 로그인 구현 필수 (구현 안할 시 리젝 사유 ⛔️)
+ (ex. 인스타그램은 북꺼니까(?) 애플 안붙여도 괜찮음!)
+ 자체 로그인만 구성이 되어 있다면, 애플 로그인 구현 필수 아님
+ => 개인 개발자 계정이 있어야 테스트 가능
+ */
+
+final class ViewController: UIViewController {
+    
+    //MARK: - UI
+    @IBOutlet private var appleLoginButton: ASAuthorizationAppleIDButton!
+    
+    //MARK: - View Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    //MARK: - Private Methods
+    private func configureUI() {
+        view.backgroundColor = .systemYellow.withAlphaComponent(0.6)
+        
+        appleLoginButton.addTarget(
+            self,
+            action: #selector(appleLoginButtonClicked),
+            for: .touchUpInside
+        )
+    }
+    
+    //MARK: - Action
+    @objc private func appleLoginButtonClicked() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.email, .fullName]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+}
+
+//MARK: - Delegate ASAuthorizationController
+extension ViewController: ASAuthorizationControllerDelegate {
+    
+    /// Apple Login Success
+    /// 첫 번째 시도: 계속 Email, fullName 제공
+    /// 두 번째 시도: 로그인 할래요? Email, fullName이 nil 값으로 옴
+    /// 사용자 정보를 계속 제공해주지는 않음 -> 최초만 제공 ⭐️
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        print(#function)
+        
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            print("appleIDCredential: ", appleIDCredential)
+            
+            let user = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            UserDefaults.standard.setValue(user, forKey: "User")
+            
+            guard
+                let identityToken = appleIDCredential.identityToken,
+                let tokenToString = String(data: identityToken, encoding: .utf8)
+            else {
+                print("TOKEN ERROR")
+                return
+            }
+            
+            print("INFO: ")
+            print(user)
+            print(fullName ?? "No fullName")
+            print(email ?? "No email")
+            print(identityToken)
+            print("------------------------------")
+            
+            if email?.isEmpty ?? true {
+                let decodedToken = decode(jwtToken: tokenToString)["email"] as? String ?? ""
+                print("Token DECODED: ")
+                print(decodedToken)
+                print()
+            }
+            
+            /// 이메일 / 토큰 / 이름 -> UserDefaults & API 서버로 POST
+            /// 서버에 Request에 대한 Response를 받으면 성공 시 화면 전환
+            DispatchQueue.main.async {
+                self.present(MainViewController(), animated: true)
+            }
+            
+            return
+        case let passwordCredential as ASPasswordCredential:
+            
+            let user = passwordCredential.user
+            let password = passwordCredential.password
+            
+            print("passwordCredential: ", passwordCredential)
+            print(user)
+            print(password)
+            print("------------------")
+            
+            return
+        default:
+            break
+        }
+    }
+    
+    /// Apple Login Failed
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(#function)
+        print("LOGIN FAILED: \(error.localizedDescription)")
+    }
+}
+
+//MARK: - ASAuthorizationControllerPresentationContextProviding
+extension ViewController: ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        if let viewWindow = self.view.window {
+            return viewWindow
+        }
+        return ASPresentationAnchor()
+    }
+}
+
+//MARK: - Decode
+private extension ViewController{
+    
+    func decode(jwtToken jwt: String) -> [String: Any] {
+        
+        func base64UrlDecode(_ value: String) -> Data? {
+            var base64 = value
+                .replacingOccurrences(of: "-", with: "+")
+                .replacingOccurrences(of: "_", with: "/")
+            
+            let length = Double(base64.lengthOfBytes(using: String.Encoding.utf8))
+            let requiredLength = 4 * ceil(length / 4.0)
+            let paddingLength = requiredLength - length
+            if paddingLength > 0 {
+                let padding = "".padding(toLength: Int(paddingLength), withPad: "=", startingAt: 0)
+                base64 = base64 + padding
+            }
+            return Data(base64Encoded: base64, options: .ignoreUnknownCharacters)
+        }
+        
+        func decodeJWTPart(_ value: String) -> [String: Any]? {
+            guard let bodyData = base64UrlDecode(value),
+                  let json = try? JSONSerialization.jsonObject(with: bodyData, options: []), let payload = json as? [String: Any] else {
+                return nil
+            }
+            
+            return payload
+        }
+        
+        let segments = jwt.components(separatedBy: ".")
+        
+        return decodeJWTPart(segments[1]) ?? [:]
+    }
+}
